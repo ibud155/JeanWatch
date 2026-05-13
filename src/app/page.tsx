510dc31650c70a07ef59187118a08760fcc51224
Isaac Budgell <isaacbudgell@Isaacs-Laptop.local>
2026-05-13 13:31:41 -0400

    lowercase subtitle on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
           JeanWatch
         </h1>
         <p className="text-lg text-gray-500 dark:text-gray-400">
-          Writing about trying things.
+          writing about trying things.
         </p>
       </div>
 
